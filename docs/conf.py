45bd4345c356ce33531f2747984e124f30f206fa
rishav <rishav@pichainlabs.com>
2021-09-29 19:17:40 +0530

    readthe docs config

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('_ext'))
 
 
 # -- Project information -----------------------------------------------------
@@ -27,8 +28,8 @@ author = 'Rishav Sinha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['edit_on_github']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,3 +51,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+edit_on_github_project = 'rishav00a/django_url_shortener'
+edit_on_github_branch = 'main'
